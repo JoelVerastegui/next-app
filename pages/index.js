4ac6d9dbcdf29c92175ec57c9b3ca093188c9804
JoelVerastegui <joelverastegui99@gmail.com>
2020-08-21 22:09:27 -0500

    Removing local images and adding amazon s3 url

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const Index = () => {
     return(
         <Content>
             <div className="d-flex flex-column">
-                <div className="d-flex justify-content-center align-items-start parallax" style={{ position: 'relative', backgroundImage: 'url("drinks-bg.jpg")' }}>
+                <div className="d-flex justify-content-center align-items-start parallax" style={{ position: 'relative', backgroundImage: 'url("https://next-app-bucket-2020.s3.us-east-2.amazonaws.com/drinks-bg.jpg")' }}>
                     <div className="d-flex flex-column justify-content-center align-items-center w-100" style={{ height: '90vh', backgroundColor: '#00000080' }}>
                         <h1 className="text-white text-center" style={{ fontSize: '7vmin' }}>¡Tenemos la que buscas!</h1>
                         <h4 className="text-white text-center" style={{ fontSize: '3.8vmin' }}>Las mejores bebidas al mejor precio en un solo lugar!</h4>
@@ -15,13 +15,13 @@ const Index = () => {
                     <h1 className="titulo">Bebidas</h1>
                     <div className="row d-flex justify-content-center align-items-center">
                         <div className="col-md-3 p-4" style={{ maxWidth: '18.125rem', minWidth: '18.125rem' }}>
-                            <DrinkCard img="drink-1.png" name="Jameson" price="170.00" />
+                            <DrinkCard img="https://next-app-bucket-2020.s3.us-east-2.amazonaws.com/drink-1.png" name="Jameson" price="170.00" />
                         </div>
                         <div className="col-md-3 p-4" style={{ maxWidth: '18.125rem', minWidth: '18.125rem' }}>
-                            <DrinkCard img="drink-2.png" name="Jack Daniel's" price="160.00" />
+                            <DrinkCard img="https://next-app-bucket-2020.s3.us-east-2.amazonaws.com/drink-2.png" name="Jack Daniel's" price="160.00" />
                         </div>
                         <div className="col-md-3 p-4" style={{ maxWidth: '18.125rem', minWidth: '18.125rem' }}>
-                            <DrinkCard img="drink-3.png" name="Johnnie Walker" price="175.00" />
+                            <DrinkCard img="https://next-app-bucket-2020.s3.us-east-2.amazonaws.com/drink_3.jpg" name="Johnnie Walker" price="175.00" />
                         </div>
                         <div className="col-md-3 p-4" style={{ maxWidth: '18.125rem', minWidth: '18.125rem' }}>
                             <DrinkCard name="Bebida 2" price="170.00" />
