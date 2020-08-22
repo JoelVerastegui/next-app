48ff3696ff2748285364d32c722023bb9c320cea
JoelVerastegui <joelverastegui99@gmail.com>
2020-08-21 19:16:29 -0500

    Adding deploy to heroku action

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Index = () => {
                 <div className="d-flex justify-content-center align-items-start parallax" style={{ position: 'relative', backgroundImage: 'url("drinks-bg.jpg")' }}>
                     <div className="d-flex flex-column justify-content-center align-items-center w-100" style={{ height: '90vh', backgroundColor: '#00000080' }}>
                         <h1 className="text-white text-center" style={{ fontSize: '7vmin' }}>¡Tenemos la que buscas!</h1>
-                        <h4 className="text-white text-center" style={{ fontSize: '3.8vmin' }}>Las mejores bebidas al mejor precio en un solo lugar</h4>
+                        <h4 className="text-white text-center" style={{ fontSize: '3.8vmin' }}>Las mejores bebidas al mejor precio en un solo lugar!</h4>
                     </div>
                 </div>
                 <div className="m-5">
@@ -18,7 +18,7 @@ const Index = () => {
                             <DrinkCard img="drink-1.png" name="Jameson" price="170.00" />
                         </div>
                         <div className="col-md-3 p-4" style={{ maxWidth: '18.125rem', minWidth: '18.125rem' }}>
-                            <DrinkCard name="Cartavio" price="170.52" />
+                            <DrinkCard img="drink-2.png" name="Jack Daniel's" price="160.00" />
                         </div>
                         <div className="col-md-3 p-4" style={{ maxWidth: '18.125rem', minWidth: '18.125rem' }}>
                             <DrinkCard name="Bebida 1" price="170.00" />
